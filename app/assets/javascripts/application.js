7f2f055feb23d575e9a1a00d8f4087a6cd1c192c
Jan Riethmayer <jan@riethmayer.de>
2011-11-15 14:41:43 +0100

    translate javascript to jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-//= require prototype
-//= require prototype_ujs
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
